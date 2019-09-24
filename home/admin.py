2a689fca9f16add0c277e21c487c45b97b602967
arihantjain8118 <arihantjain8118@gmail.com>
2019-09-24 17:30:52 +0530

    models

--- home/admin.py
+++ home/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
+
+
+from .models import Train,Seat,Ticket
+
+admin.site.register(Train)
+admin.site.register(Seat)
+admin.site.register(Ticket)
--- home/models.py
+++ home/models.py
@@ -1,3 +1,34 @@
 from django.db import models
 
+
+from django.contrib.auth.models import User
 # Create your models here.
+class Train(models.Model):
+    name = models.CharField(max_length=100)
+    number = models.CharField(max_length=100)
+    source = models.CharField(max_length=100)
+    destination = models.CharField(max_length=100)
+    departure_time = models.TimeField(max_length=100)
+    arrival_time = models.TimeField(max_length=100)
+    status = models.CharField(max_length=100)
+    
+    def __str__(self):
+        return "%s's Train" % self.name
+
+class Seat(models.Model):
+    seat_id = models.CharField(max_length=100)
+    train = models.ForeignKey(Train,on_delete =models.CASCADE)
+    status = models.CharField(max_length=100)
+    date = models.DateField()
+  
+    def __str__(self):
+        return "%s's Seat" % self.seat_id
+
+class Ticket(models.Model):
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
+    pnr = models.CharField(max_length=100)
+    status = models.CharField(max_length=100)
+
+
+    def __str__(self):
+        return "%s's Ticket" % self.user
