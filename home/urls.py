7b30a6a2f69f0f07a6b8f649e7cb51c1a396eca0
arihantjain8118 <arihantjain8118@gmail.com>
2019-09-26 13:35:24 +0530

    search trains

--- home/urls.py
+++ home/urls.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
-from .views import starting
+from .views import starting,trains
 
 
 urlpatterns = [
     path('', starting, name="home-home"),
+    path('trains/',trains,name="home-trains")
 ]
--- home/views.py
+++ home/views.py
@@ -1,5 +1,27 @@
 from django.shortcuts import render
-
+from .models import Train
+from django.template.loader import render_to_string
+from django.http import HttpResponse,HttpResponseRedirect,JsonResponse
 # Create your views here.
 def starting(request):
-    return render(request,'home/starting.html',{})
+    user=request.user
+    print("jdcncj")
+    return render(request,'home/starting.html',{'user':user})
+
+def trains(request):
+    
+    if request.is_ajax():
+        print("aaya")
+        source = request.POST.get('source',None)
+        destination = request.POST.get('destination',None)
+        print(source)
+        print(destination)
+        trains=[]
+        ptrains = (Train.objects.filter(source=source))
+        for i in ptrains:
+            if(i.destination == destination):
+                trains.append(i)
+
+        context={'trains':trains}
+        html = render_to_string('home/trains.html',context,request=request)
+        return JsonResponse({'form': html})
