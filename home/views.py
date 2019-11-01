ab3858b192afd57a068ba4ab48b68ef235ecbbe2
arihantjain8118 <arihantjain8118@gmail.com>
2019-11-01 20:20:46 +0530

    booking

--- home/views.py
+++ home/views.py
@@ -35,6 +35,42 @@ def ticketbook(request,id):
     train = Train.objects.filter(id=id).first()
     return render(request,'home/book.html',{'train':train})
 
-def ticketbk(request):
+def ticketbk(request,id):
+    print("aayay habhah")    
     form = ticket()
     return render(request,'home/tick.html',{'form':form})
+
+def realbook(request,id):
+    if request.method == 'POST':
+        user=request.user
+        train = Train.objects.filter(id=id).first()
+        date = request.POST.get('date',None)
+        tcount = Ticket.objects.filter(date=date)
+        g=0
+        for i in tcount:
+                if i.train == train:
+                        g=g+1
+        if(g>60):
+                status = "Waiting List"
+        # return render(request,'home/starting.html')
+        else:
+                status = "Confirm"
+        o = str(date)
+        p = o[0:4]
+        p = p + o[5:7]
+        p = p + o[8:11]
+        z = Ticket.objects.all()
+        po = z.count()
+        po = str(po)
+        print(po)
+        pnr = str(train.id)+p+str(user.id)+po
+        
+        passenger_name = request.POST.get('name',None)
+        age = request.POST.get('age',None)
+        ticket = Ticket(user=user,
+        pnr=pnr,status=status,date=date,train=train,passenger_name=passenger_name,
+        age=age
+        )
+        ticket.save()
+        print(passenger_name)
+        return render(request,'home/starting.html')
