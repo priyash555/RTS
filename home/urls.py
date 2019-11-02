e8fecc7d18b3e27a48e8012fed4f2f3dfdfc237e
priyash <priyashgpt2@gmail.com>
2019-11-02 07:32:13 +0530

    style

--- home/urls.py
+++ home/urls.py
@@ -1,11 +1,13 @@
 from django.contrib import admin
 from django.urls import path, include
-from .views import starting,trains,ticketbook,ticketbk
+from .views import starting,trains,ticketbook,ticketbk,realbook
 
 
 urlpatterns = [
     path('', starting, name="home-home"),
     path('trains/',trains,name="home-trains"),
-    path('book/<int:id>',ticketbook,name="home-book"),
-    path('tickbk/',ticketbk,name="home-bk")
+    path('book/<int:id>/',ticketbook,name="home-book"),
+    path('book/<int:id>/form/',ticketbk,name="home-bookform"),
+    path('tickbk/',ticketbk,name="home-bk"),
+    path('book/<int:id>/form/realbook/',realbook,name="home-realbook")
 ]
